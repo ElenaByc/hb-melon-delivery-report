655045cc15fc35c9f6df39c59347050a3b07c919
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-07 15:46:02 -0700

    feat: implement the print_day_delivery_report function

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,19 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+def print_day_delivery_report(input_file, delivey_day):
+    """Produce delivery report for the given day from the given file with data"""
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+    # Ali Baba Watermelon|18|25.00
+    print(f"Day {delivey_day}")
+    delivery_data = open(input_file)
 
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+    for line in delivery_data:
+        melon, count, amount = line.rstrip().split('|')
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+        
+    delivery_data.close()
 
 
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+print_day_delivery_report("um-deliveries-day-1.txt", 1)
+print_day_delivery_report("um-deliveries-day-2.txt", 2)
+print_day_delivery_report("um-deliveries-day-3.txt", 3)
